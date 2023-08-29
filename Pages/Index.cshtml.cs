cfa01855ed8007f80fe540a8db1584d1e54edf91
Jogchem Andre Oord <jogchem.oord@emergo.nl>
2023-08-29 16:00:25 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty(SupportsGet = true)]
+        public string From { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string To { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int? MinFlightTime { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int? MaxFlightTime { get; set; }
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
